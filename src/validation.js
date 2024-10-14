a4267ad7824b82d97d5b5a546c0c77a5e45eca4b
ltxstudio <ltxstudio.info@gmail.com>
2024-10-15 01:55:03 +0600

    Update validation.js

--- src/validation.js
+++ src/validation.js
@@ -1,15 +1,26 @@
 // src/validation.js
-export const validateCardNumber = (number) => {
-  const regex = /^[0-9]{16}$/;
-  return regex.test(number);
+export const validateCardNumber = (cardNumber) => {
+  const regex = /^\d{16}$/;
+  return regex.test(cardNumber);
 };
 
 export const validateExpiryDate = (expiry) => {
   const regex = /^(0[1-9]|1[0-2])\/\d{4}$/;
-  return regex.test(expiry);
+  if (!regex.test(expiry)) return false;
+  
+  const [month, year] = expiry.split('/').map(Number);
+  const now = new Date();
+  const currentYear = now.getFullYear();
+  const currentMonth = now.getMonth() + 1;
+  
+  if (year < currentYear || (year === currentYear && month < currentMonth)) {
+    return false;
+  }
+
+  return true;
 };
 
 export const validateCVV = (cvv) => {
-  const regex = /^[0-9]{3}$/;
+  const regex = /^\d{3}$/;
   return regex.test(cvv);
 };
