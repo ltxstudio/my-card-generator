2e8ffbc5165d275bba6c4eabedc3851a82416192
ltxstudio <ltxstudio.info@gmail.com>
2024-10-15 02:11:24 +0600

    Update generateDetails.js

--- src/generateDetails.js
+++ src/generateDetails.js
@@ -1,4 +1,5 @@
 // src/generateDetails.js
+
 export const generateExpiryDate = () => {
   const currentYear = new Date().getFullYear();
   const year = currentYear + Math.floor(Math.random() * 5);
@@ -8,4 +9,29 @@ export const generateExpiryDate = () => {
 
 export const generateCVV = () => {
   return String(Math.floor(Math.random() * 900) + 100); // Generate a 3-digit CVV
+};
+
+export const validateCardDetails = (number, expiry, cvv) => {
+  // Validate card number using Luhn algorithm
+  const luhnCheck = (num) => {
+    let sum = 0;
+    for (let i = 0; i < num.length; i++) {
+      let digit = parseInt(num[i]);
+      if ((num.length - i) % 2 === 0) digit *= 2;
+      if (digit > 9) digit -= 9;
+      sum += digit;
+    }
+    return sum % 10 === 0;
+  };
+
+  // Validate expiry date format and value
+  const expiryCheck = /^\d{2}\/\d{4}$/.test(expiry);
+  const [month, year] = expiry.split('/').map(Number);
+  const validMonth = month >= 1 && month <= 12;
+  const validYear = year >= new Date().getFullYear();
+
+  // Validate CVV format and value
+  const cvvCheck = /^\d{3}$/.test(cvv);
+
+  return luhnCheck(number) && expiryCheck && validMonth && validYear && cvvCheck;
 };
